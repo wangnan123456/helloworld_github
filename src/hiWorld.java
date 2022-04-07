2239756b61461a416cabae54fa8dbd2ff5c53b6f
wangnan_glb <wangnan_glb@qq.com>
2022-04-07 15:54:26 +0800

    test

---
+++ src/hiWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @Auther: wangnan
+ * @Date: 2022/4/7
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class hiWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hi world !!");
+    }
+}
